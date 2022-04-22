abc3e67c711b5866bdd30ad11027bbd85098ba32
Enzo Pigatti <enzoxz.023@gmail.com>
2022-04-22 00:34:43 -0400

    test

--- src/Utils/baseUrl.js
+++ src/Utils/baseUrl.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "http://localhost:8000";
-export const baseUrl = "https://backpuntoferretero.herokuapp.com";
+//export const baseUrl = "https://backpuntoferretero.herokuapp.com";
+export const baseUrl = "http://finanzasapi.enzopigatti.cl";
